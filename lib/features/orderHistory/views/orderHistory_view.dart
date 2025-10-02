9e7c0de431fdc61c301ef04c46e5ff2de2b0f450
Ahmed Abdelgawad <hagahm721@gmail.com>
2025-10-02 15:56:13 +0300

    finish OrderHisrtory view UI

--- lib/features/orderHistory/views/orderHistory_view.dart
+++ lib/features/orderHistory/views/orderHistory_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:gap/gap.dart';
+import 'package:hungry/shared/customButton.dart';
+import 'package:hungry/shared/custom_text.dart';
 
 class OrderhistoryView extends StatefulWidget {
   const OrderhistoryView({super.key});
@@ -9,11 +11,50 @@ class OrderhistoryView extends StatefulWidget {
 }
 
 class _OrderhistoryViewState extends State<OrderhistoryView> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Order History"),),
+      appBar: AppBar(backgroundColor: Colors.white),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: ListView.builder(
+          itemCount: 4,
+          padding: EdgeInsets.only(bottom: 120, top: 10),
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 20,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Image.asset('assets/test/image6.png', width: 100),
+                        Spacer(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomText(text: 'Hamburger Hamburger', fontSize: 16),
+                            CustomText(text: 'Qty : x3',fontWeight: FontWeight.w700,fontSize: 13,),
+                            CustomText(text: 'Price : 20\$',fontWeight: FontWeight.w900,fontSize: 13,),
+                          ],
+                        ),
+                      
+                      ],
+                    ),
+                    Gap(20),
+                    Custombutton(text: 'Order Again', ontap: (){},width: double.infinity,color: Colors.grey.shade400,)
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
--- lib/shared/customButton.dart
+++ lib/shared/customButton.dart
@@ -3,19 +3,23 @@ import 'package:hungry/core/constants/app_colors.dart';
 import 'package:hungry/shared/custom_text.dart';
 
 class Custombutton extends StatelessWidget {
-  const Custombutton({super.key, required this.text, required this.ontap});
+  const Custombutton({super.key, required this.text, required this.ontap,  this.width, this.color});
   final String text;
   final VoidCallback ontap;
+  final double? width;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return  GestureDetector(
       onTap: ontap,
       child: Container(
+        alignment: Alignment.center,
+        width: width,
                     padding: EdgeInsets.symmetric(vertical: 15,horizontal: 25
                     ),
                     decoration: BoxDecoration(
-                      color: AppColors.primaryColor,
+                      color:color?? AppColors.primaryColor,
                       borderRadius: BorderRadius.circular(12),
                     ),
                     child: CustomText(text: text,color: Colors.white,),
